aaad41a5faf867061be3e561707dd141047785bf
Werner Van Aswegen <werner.vanaswegen@gmail.com>
2016-06-16 13:19:01 +0200

    Made changes to project folder structure and added Yeoman, Grunt and Karma

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name Group1WebApp
+ * @description
+ * # Group1WebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module("Group1WebApp", [
+    'ui.router',
+    'ngAnimate'//,
+    //'ngCookies',
+    //'ngResource',
+    //'ngSanitize',
+    //'ngTouch'
+  ])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/notfound');
+
+    $stateProvider
+      .state("notfound", {
+        url: "/notfound",
+        views: {
+          '': { templateUrl: "views/graph-not-found.html" },
+          'menu@notfound': { templateUrl: "views/menu.html" }
+        }
+      })
+      .state("graph", {
+        url: '/graph/:graphName',
+        views: {
+          "": {templateUrl: "views/single-graph.html", controller: "SingleGraphCtrl"},
+          "menu@graph": {templateUrl: "views/menu.html"}
+        }
+      });
+  });
