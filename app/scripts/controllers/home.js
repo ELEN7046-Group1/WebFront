2a21f170dff7a6f7f3bee434fd6ddfc64e996ea2
wernerva <werner.vanaswegen@gmail.com>
2016-06-21 15:01:39 +0200

    added loader image

---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webFrontApp.controller:HomeCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the webFrontApp
+ */
+angular.module('Group1WebApp')
+  .controller('HomeCtrl', function () {
+    
+  });
